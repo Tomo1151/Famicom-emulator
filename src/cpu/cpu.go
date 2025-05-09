80151cdb2918361e407ed50868241d0d04ad1a4d
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-05-09 17:13:00 +0900

    コメントを追加

--- src/cpu/cpu.go
+++ src/cpu/cpu.go
@@ -1,15 +1,20 @@
 package cpu
 
+import "fmt"
+
+// CPUの定義
 type CPU struct {
 	Registers registers
 }
 
+// CPUの作成関数
 func CreateCPU() CPU {
 	cpu := CPU{}
 	cpu.Init()
 	return cpu
 }
 
+// CPUの初期化メソッド
 func (c *CPU) Init() {
 	c.Registers = registers{
 		A: 0x00,
@@ -27,5 +32,13 @@ func (c *CPU) Init() {
 		},
 		SP: 0xFD,
 		PC: 0x0000,
+	}
+}
+
+// 命令の実行
+func (c * CPU) execute(opecode uint8) {
+	switch opecode {
+	default:
+		fmt.Println(opecode)
 	}
 }
