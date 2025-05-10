7f82c2b5b659fe0a1e5d7eda3d4bba5d61be9af7
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-05-10 19:12:50 +0900

    命令の定義と雛形を作成

--- src/cpu/cpu.go
+++ src/cpu/cpu.go
@@ -4,18 +4,23 @@ import "fmt"
 
 // CPUの定義
 type CPU struct {
-	Registers registers
+	Registers registers // レジスタ
+	InstructionSet instructionSet // 命令セット
+
+	wram [0xFFFF]uint8 // WRAM
+
+	log bool // デバッグ出力フラグ
 }
 
 // CPUの作成関数
-func CreateCPU() CPU {
+func CreateCPU(debug bool) CPU {
 	cpu := CPU{}
-	cpu.Init()
+	cpu.Init(debug)
 	return cpu
 }
 
 // CPUの初期化メソッド
-func (c *CPU) Init() {
+func (c *CPU) Init(debug bool) {
 	c.Registers = registers{
 		A: 0x00,
 		X: 0x00,
@@ -33,12 +38,42 @@ func (c *CPU) Init() {
 		SP: 0xFD,
 		PC: 0x0000,
 	}
+	c.InstructionSet = generateInstructionSet(c)
+	c.log = debug
 }
 
 // 命令の実行
-func (c * CPU) execute(instruction Instruction) {
-	switch instruction.Code {
-	default:
-		fmt.Println(instruction.Code, ": ", instruction.Opecode)
+func (c *CPU) Execute() {
+	opecode := c.ReadWRAM(c.Registers.PC)
+	instruction := c.InstructionSet[opecode]
+
+	if c.log {
+		fmt.Printf("opecode: $%02X (%s) from: $%04X\n", opecode, instruction.Code.ToString(), c.Registers.PC)
+		fmt.Println("instr: ", instruction)
+	}
+
+	instruction.Handler(instruction.AddressingMode, 0)
+
+	// プログラムカウンタを進める
+	c.Registers.PC = uint16(c.Registers.PC + uint16(instruction.Bytes))
+
+	if c.log {
+		fmt.Printf("PC: $%04X\n\n", c.Registers.PC)
+	}
+}
+
+// ワーキングメモリの参照
+func (c *CPU) ReadWRAM(address uint16) uint8 {
+	return c.wram[address]
+}
+
+// ワーキングメモリへの書き込み
+func (c *CPU) WriteWRAM(address uint16, data uint8) {
+	c.wram[address] = data
+}
+
+func (c *CPU) adc(addressing AddressingMode, operand uint16) {
+	if c.log {
+		fmt.Printf("*ADC* mode: $%02X operand: $%04X", addressing, operand)
 	}
 }
--- src/cpu/instructions.go
+++ src/cpu/instructions.go
@@ -1,7 +1,136 @@
 package cpu
 
+import "fmt"
+
 type Instruction struct {
-	Code uint8
-	Opecode uint8
-	AddressingMode uint8
+	Opecode uint8 // オペコード
+	Code InstructionCode // 命令の種類
+	AddressingMode AddressingMode // アドレッシングモード
+	Bytes uint8 // 命令のバイト数
+	Cycles uint8 // 基本サイクル数
+	PageCycles uint8 // ページ協会を越えた場合の追加サイクル
+	Handler InstructionHandler // 命令の実装
+}
+
+// 命令の種類の型定義
+type InstructionCode uint8
+
+// アドレッシングモード型の定義
+type AddressingMode uint8
+
+type InstructionHandler func(addressing AddressingMode, operand uint16)
+
+// 命令セット型の定義
+type instructionSet map[uint8]Instruction
+
+
+
+// 定数の定義
+const (
+	Implied AddressingMode = iota // impl
+	Accumulator // A
+	Immediate // #
+	ZeroPage // zpg
+	ZeroPageXIndexed // zpg,X
+	ZeroPageYIndexed // zpg,Y
+	Absolute // abs
+	AbsoluteXIndexed // abs,X
+	AbsoluteYIndexed // abs,Y
+	Relative // rel
+	Indirect // Ind
+	IndirectXIndexed // X,Ind
+	IndirectYIndexed // Ind,Y
+)
+
+
+const (
+	ADC InstructionCode = iota
+	AND
+	ASL
+	BCC
+	BCS
+	BEQ
+	BIT
+	BMI
+	BNE
+	BPL
+	BRK
+	BVC
+	BVS
+	CLC
+	CLD
+	CLI
+	CLV
+	CMP
+	CPX
+	CPY
+	DEC
+	DEX
+	DEY
+	EOR
+	INC
+	INX
+	INY
+	JMP
+	JSR
+	LDA
+	LDX
+	LDY
+	LSR
+	NOP
+	ORA
+	PHA
+	PHP
+	PLA
+	PLP
+	ROL
+	ROR
+	RTI
+	RTS
+	SBC
+	SEC
+	SED
+	SEI
+	STA
+	STX
+	STY
+	TAX
+	TAY
+	TSX
+	TXA
+	TXS
+	TYA
+)
+
+
+// 命令セットの生成
+func generateInstructionSet(c *CPU) instructionSet {
+	instructionSet := make(instructionSet)
+
+	// ADC命令
+	instructionSet[0x69] = Instruction{
+		Opecode: 0x69,
+		Code: ADC,
+		AddressingMode: Immediate,
+		Bytes: 2,
+		Cycles: 2,
+		PageCycles: 1,
+		Handler: c.adc,
+	}
+
+	return instructionSet
+}
+
+
+// 命令名取得メソッド
+func (ic InstructionCode) ToString() string {
+	names := [...]string {
+		"ADC", "AND", "ASL", "BCC", "BCS", "BEQ",	"BIT", "BMI","BNE", "BPL",	"BRK","BVC","BVS","CLC","CLD","CLI","CLV","CMP","CPX","CPY","DEC","DEX","DEY","EOR","INC","INX","INY","JMP","JSR","LDA","LDX","LDY","LSR","NOP","ORA","PHA","PHP","PLA","PLP","ROL","ROR","RTI","RTS","SBC","SEC","SED","SEI","STA","STX","STY","TAX","TAY","TSX","TXA","TXS","TYA",
+	}
+
+	if int(ic) < len(names) {
+		return names[ic]
+	} else {
+		return fmt.Sprintf("Unknown(%d)", ic)
+	}
 }
