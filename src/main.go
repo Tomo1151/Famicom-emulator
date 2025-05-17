536b1fa54b230985def572463ca964dd1bd01597
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-05-18 01:49:57 +0900

    不要な記述を削除，デバッグ用メソッドCPU.Trace() を使用するように変更

--- src/main.go
+++ src/main.go
@@ -8,29 +8,23 @@ import (
 	"math/rand"
 	"os"
 	"time"
-	"unsafe"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
-
 	filedata, err := os.ReadFile("../rom/nestest.nes")
 
 	if err != nil {
 		log.Fatalf("Error occured in 'os.ReadFile()'")
 	}
 
-	// fmt.Println(filedata)
 
 	cart := cartridge.Cartridge{}
 	err = cart.Load(filedata)
 	if err != nil {
 		log.Fatalf("Cartridge loading error: %v", err)
 	}
-
-
 
 	runCPUTestGame(&cart)
 }
@@ -96,6 +90,8 @@ func createWindow(c *cpu.CPU) {
 
 	// メインループ
 	c.Run(func(c *cpu.CPU) {
+		fmt.Println(c.Trace())
+
 		// イベント処理
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch e := event.(type) {
@@ -105,41 +101,9 @@ func createWindow(c *cpu.CPU) {
 				if e.Keysym.Sym == sdl.K_ESCAPE && e.State == sdl.PRESSED {
 					running = false
 				}
-
-				// キー入力の処理（押された時のみ）
-				if e.State == sdl.PRESSED {
-					switch e.Keysym.Sym {
-					case sdl.K_w:
-						c.WriteByteAt(0xFF, 0x77)
-					case sdl.K_s:
-						c.WriteByteAt(0xFF, 0x73)
-					case sdl.K_a:
-						c.WriteByteAt(0xFF, 0x61)
-					case sdl.K_d:
-						c.WriteByteAt(0xFF, 0x64)
-					default:
-					}
-				}
 			}
 		}
 
-		// 0xFEに乱数を書き込み（1-15の範囲）
-		c.WriteByteAt(0xFE, uint8(rand.Intn(15)+1))
-
-		// 画面状態の更新をチェックし、変更があれば描画
-		if readScreenState(c, &screenState) {
-			// テクスチャを更新
-			texture.Update(nil, unsafe.Pointer(&screenState), 32*3)
-
-			// レンダラーをクリア
-			renderer.Clear()
-
-			// テクスチャをレンダラーにコピー
-			renderer.Copy(texture, nil, nil)
-
-			// 画面に表示
-			renderer.Present()
-		}
 
 		// CPUのフレームレート調整
 		time.Sleep(70 * time.Microsecond)
@@ -152,55 +116,12 @@ func createWindow(c *cpu.CPU) {
 	})
 }
 
-func readScreenState(c *cpu.CPU, frame *[32 * 3 * 32]uint8) bool {
-	frameIdx := 0
-	update := false
-	for i := 0x0200; i < 0x0600; i++ {
-		colorIdx := c.ReadByteFrom(uint16(i))
-		col := color(colorIdx)
-		if frame[frameIdx] != col.R || frame[frameIdx+1] != col.G || frame[frameIdx+2] != col.B {
-			frame[frameIdx] = col.R
-			frame[frameIdx+1] = col.G
-			frame[frameIdx+2] = col.B
-			update = true
-		}
-
-		frameIdx += 3
-	}
-
-	return update
-}
-
-func color(bytes uint8) sdl.Color {
-	switch bytes {
-	case 0:
-		return sdl.Color{R: 0, G: 0, B: 0, A: 255} // BLACK
-	case 1:
-		return sdl.Color{R: 255, G: 255, B: 255, A: 255} // WHITE
-	case 2, 9:
-		return sdl.Color{R: 128, G: 128, B: 128, A: 255} // GREY
-	case 3, 10:
-		return sdl.Color{R: 255, G: 0, B: 0, A: 255} // RED
-	case 4, 11:
-		return sdl.Color{R: 0, G: 255, B: 0, A: 255} // GREEN
-	case 5, 12:
-		return sdl.Color{R: 0, G: 0, B: 255, A: 255} // BLUE
-	case 6, 13:
-		return sdl.Color{R: 255, G: 0, B: 255, A: 255} // MAGENTA
-	case 7, 14:
-		return sdl.Color{R: 255, G: 255, B: 0, A: 255} // YELLOW
-	default:
-		return sdl.Color{R: 0, G: 255, B: 255, A: 255} // CYAN
-	}
-}
-
 // デバッグ用スネークゲーム
 func runCPUTestGame(cartridge *cartridge.Cartridge) {
 	c := cpu.CreateCPU(true)
 
 	c.InitWithCartridge(cartridge, true)
-
-	fmt.Printf("Entry point: $%04X\n", c.ReadByteFrom(0xFFFC))
+	c.Registers.PC = 0xC000
 
 	// ウィンドウとレンダラーを準備
 	createWindow(c)
