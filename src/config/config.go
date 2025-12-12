ec7d0d368791411e5e5f77c1723a169c44a3a8d1
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-12-12 10:28:52 +0900

    config.json からキーバインドを変えられるように変更

--- src/config/config.go
+++ src/config/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -15,7 +16,7 @@ const (
 
 // MARK: デフォルトのキーコンフィグ
 var DefaultControl = ControlConfig{
-	KEY_1P: KeyConfig{
+	KEY_1P: SDLKeyConfig{
 		BUTTON_A:      sdl.K_k,
 		BUTTON_B:      sdl.K_j,
 		BUTTON_UP:     sdl.K_w,
@@ -25,7 +26,7 @@ var DefaultControl = ControlConfig{
 		BUTTON_START:  sdl.K_RETURN,
 		BUTTON_SELECT: sdl.K_BACKSPACE,
 	},
-	KEY_2P: KeyConfig{
+	KEY_2P: SDLKeyConfig{
 		BUTTON_A:      sdl.K_SLASH,
 		BUTTON_B:      sdl.K_PERIOD,
 		BUTTON_UP:     sdl.K_g,
@@ -70,13 +71,15 @@ type RenderConfig struct {
 
 // MARK: ControllerConfigの定義
 type ControlConfig struct {
-	KEY_1P               KeyConfig `json:"key1p"`
-	KEY_2P               KeyConfig `json:"key2p"`
-	GamepadAxisThreshold int16     `json:"gamepadAxisThreshold"`
+	RAW_KEY_1P           KeyConfig `json:"key1p"`
+	RAW_KEY_2P           KeyConfig `json:"key2p"`
+	KEY_1P               SDLKeyConfig
+	KEY_2P               SDLKeyConfig
+	GamepadAxisThreshold int16 `json:"gamepadAxisThreshold"`
 }
 
 // MARK: KeyConfigの定義
-type KeyConfig struct {
+type SDLKeyConfig struct {
 	BUTTON_A      sdl.Keycode
 	BUTTON_B      sdl.Keycode
 	BUTTON_UP     sdl.Keycode
@@ -85,6 +88,18 @@ type KeyConfig struct {
 	BUTTON_LEFT   sdl.Keycode
 	BUTTON_START  sdl.Keycode
 	BUTTON_SELECT sdl.Keycode
+}
+
+// MARK: KeyConfigの定義
+type KeyConfig struct {
+	BUTTON_A      string `json:"buttonA"`
+	BUTTON_B      string `json:"buttonB"`
+	BUTTON_UP     string `json:"buttonUp"`
+	BUTTON_DOWN   string `json:"buttonDown"`
+	BUTTON_RIGHT  string `json:"buttonRight"`
+	BUTTON_LEFT   string `json:"buttonLeft"`
+	BUTTON_START  string `json:"buttonStart"`
+	BUTTON_SELECT string `json:"buttonSelect"`
 }
 
 // MARK: コンフィグファイルの読み込み
@@ -103,7 +118,7 @@ func LoadFromFile() *Config {
 		fmt.Println("Config file contains invalid field.")
 
 		// パースに失敗した場合は最低限の設定で起動
-		config = &Config{
+		config = Config{
 			Render: RenderConfig{
 				SCALE_FACTOR: 3,
 			},
@@ -111,17 +126,36 @@ func LoadFromFile() *Config {
 		}
 	}
 
-	return config
+	return &config
 }
 
 // MARK: JSONからConfig構造体へ変換
-func ParseFromJson(file []byte) (*Config, error) {
+func ParseFromJson(file []byte) (Config, error) {
 	var config Config
 	err := json.Unmarshal(file, &config)
 
-	// キーコンフィグだけデフォルトから設定
-	config.Control.KEY_1P = DefaultControl.KEY_1P
-	config.Control.KEY_2P = DefaultControl.KEY_2P
+	// キーコンフィグをstringからsdl.KeyCodeに変換して登録
+	config.Control.KEY_1P = MapKeyConfig(config.Control.RAW_KEY_1P)
+	config.Control.KEY_2P = MapKeyConfig(config.Control.RAW_KEY_2P)
 
-	return &config, err
+	return config, err
+}
+
+// MARK: KeyConfigをSDLKeyConfigに変換
+func MapKeyConfig(raw KeyConfig) SDLKeyConfig {
+	var out SDLKeyConfig
+	rvOut := reflect.ValueOf(&out).Elem()
+	rvIn := reflect.ValueOf(raw)
+
+	for i := 0; i < rvIn.NumField(); i++ {
+		fieldName := rvIn.Type().Field(i).Name
+		rawValue := rvIn.Field(i).String()
+		outField := rvOut.FieldByName(fieldName)
+
+		if outField.IsValid() && outField.CanSet() {
+			outField.SetInt(int64(sdl.GetKeyFromName(rawValue)))
+		}
+	}
+
+	return out
 }
